ee6d238b1d6efe702432058506cb9b474a83100b
ckessinger <christian.kessinger@gmail.com>
2018-11-05 16:18:45 -0500

    added qoutes

--- js/script.js
+++ js/script.js
@@ -6,12 +6,18 @@ FSJS project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 //test
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
 
-  Recommended:
+var quotes = [
+    ['Be happy with what you have while working for what you want.', 'Helen Keller'],
+    ['Comparison is the thief of joy.', 'Theodore Roosevelt'],
+    ['in 3 words I can sum up everything I know about life: it goes on', 'Robert Frost'],
+    ['You can always do more than you think you can.', 'John Wooden'],
+    ['Getting knocked down in life is a given. Getting up and moving forward is a choice.', 'Zig Ziglar'],
+    ['If you judge people you have no time to love them.', 'Mother Teresa'],
+    ["If you can't fly, then run, if you can't run, then walk, if you can't walk, then crawl, but whatever you do, you have to keep moving forward.", "Martin Luther King Jr."]
+];
+/***
+Recommended:
     - Add at least one `year` and/or `citation` property to at least one
       quote object.
 ***/
